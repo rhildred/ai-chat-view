2e63929c22881cb503a9e337166034a83da94a14
Richard Hildred <rhildred@gmail.com>
2026-04-02 18:10:46 -0400

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import { PaperProvider } from "react-native-paper";
+import Loyalty from "./Loyalty"
+
+export default function App() {
+  return (
+<PaperProvider>
+    <Loyalty />
+</PaperProvider>  );
+}
+
